980e5f011062821e0a1fdd2006283c3d4745fc61
Sai Chandramouli <schand31@asu.edu>
2018-03-25 23:15:41 -0700

    starting with contexts

---
+++ spec/unit/app/api_spec.rb
@@ -0,0 +1,12 @@
+require 'rspec'
+
+module ExpenseTracker
+  describe API do
+    describe 'POST /expenses' do
+      context 'when the expense is successfully recorded' do
+        it 'returns the expense id'
+        it 'responds with 200 OK'
+      end
+    end
+  end
+end
