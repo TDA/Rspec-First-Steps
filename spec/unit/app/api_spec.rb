f20315dc4c0caaec9e1ad169ff0b1541d0a315ed
Sai Chandramouli <schand31@asu.edu>
2018-03-26 00:55:08 -0700

    starting with mocking for unit tests

--- spec/unit/app/api_spec.rb
+++ spec/unit/app/api_spec.rb
@@ -1,10 +1,33 @@
 require 'rspec'
+require_relative '../../../app/api'
+require 'rack/test'
 
 module ExpenseTracker
+  RecordResult = Struct.new(:success?, :expense_id, :error_message)
   describe API do
+    include Rack::Test::Methods
+
+    let(:ledger) { instance_double('ExpenseTracker::Ledger') }
+
+    def app
+      API.new(ledger: ledger)
+    end
+
     describe 'POST /expenses' do
       context 'when the expense is successfully recorded' do
-        it 'returns the expense id'
+        it 'returns the expense id' do
+          expense = { 'some' => 'data' }
+
+          allow(ledger).to receive(:record)
+            .with(expense)
+            .and_return(RecordResult.new(true, 417, nil))
+
+          post '/expenses', JSON.generate(expense)
+
+          parsed_response = JSON.parse(last_response.body)
+          expect(parsed_response).to include('expense_id' => 417)
+        end
+
         it 'responds with 200 OK'
       end
 
