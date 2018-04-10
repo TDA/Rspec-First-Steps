5d4494e024264dc7f9180d9b24f233aa6e74c042
Sai Chandramouli <schand31@asu.edu>
2018-04-10 12:48:10 -0700

    added comments

--- spec/product_spec.rb
+++ spec/product_spec.rb
@@ -1,5 +1,9 @@
 require 'rspec'
 
+# Context: Assume each of these are different classes that need to be tested,
+# since they all have similar behavior and can be run against the same examples,
+# we create a shared_example and run it against each of them.
+
 Sandwich = Struct.new(:cost, :quantity) do
   def price
     cost * quantity
