b317252422ecb98190daaf950db60a04373c069a
Sai Chandramouli <schand31@asu.edu>
2018-04-11 12:50:00 -0700

    moved into shared example

--- spec/product_spec.rb
+++ spec/product_spec.rb
@@ -28,10 +28,6 @@ end
 describe 'Product Prices' do
 
   describe Sandwich do
-    context "when the cost per item and quantity is provided" do
-      it 'should return the right price' do
-        true.should == false
-      end
-    end
+
   end
 end
---
+++ spec/support/product_shared_examples.rb
@@ -0,0 +1,9 @@
+require 'rspec'
+
+shared_examples 'Product' do
+  context 'when the cost per item and quantity is provided' do
+    it 'should return the right price' do
+      true.should == false
+    end
+  end
+end
