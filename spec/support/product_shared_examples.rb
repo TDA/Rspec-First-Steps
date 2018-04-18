89818a4e9bf488fe8901fcafdf6d2e39c2e37c29
Sai Chandramouli <schand31@asu.edu>
2018-04-18 13:34:54 -0700

    making the diff between shared_examples vs contexts clear

--- spec/support/product_shared_examples.rb
+++ spec/support/product_shared_examples.rb
@@ -3,6 +3,13 @@ require 'rspec'
 # In this shared example we are only passing in params, but these could
 # essentially be blocks or any other ruby construct.
 shared_examples 'Product' do |product_type|
+  # Keep in mind that shared_context and shared_example do the *exact* same thing behind the scenes
+  # It is a matter of communicating your intent, i.e. if a bunch of examples need to be run for different
+  # tests, use shared_examples. e.g. "If products x,y,z behave like shared_example"
+  # if a bunch of examples need a common setup/context to run, then use
+  # shared_context, e.g. "Setup 5 users for the following tests", "Setup the following helper methods for these tests"
+  before do end
+
   context 'when the cost per item and quantity is provided' do
     it 'should return the right price' do
       product = product_type.new(3, 3)
