b9f34aa9f33a3d5d9a5bd7742f54d0228acac332
Sai Chandramouli <schand31@asu.edu>
2018-04-28 23:55:06 -0700

    added rspec

--- spec/asserting_with_different_frameworks_spec.rb
+++ spec/asserting_with_different_frameworks_spec.rb
@@ -4,4 +4,8 @@ describe 'Using different assertion/expectation libraries' do
   it 'should work with minitest' do
     assert_equal 4, 4
   end
+
+  it 'should work with rspec expectations' do
+    expect(4).to eq(4)
+  end
 end
